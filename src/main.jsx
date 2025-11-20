92ae00284bbd017215d2d26d4a64e54e35bc67c6
Chetanbhat04 <chetanbhat04@gmail.com>
2025-11-20 02:45:33 -0500

    Add gh-pages deploy scripts and set vite base

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+// Import Redux Provider
 import { Provider } from 'react-redux'
+
+// Import the store
 import store from './store.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
     <App />
-    </Provider>
-  </React.StrictMode>,
+  </Provider>
 )
